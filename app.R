5161d45fbfa6f718ff9c63f572a19df717690042
djschoo <djschoo@protonmail.com>
2021-10-01 16:09:59 -0700

    default flock size is reactive, which is cool

--- app.R
+++ app.R
@@ -1,5 +1,11 @@
+# https://stackoverflow.com/questions/68550960/r-shiny-how-to-make-the-initial-value-for-numericinput-dynamic-based-on-user-i
+
 library(shiny)
+library(tidyverse)
+library(readxl)
 options(scipen = 999)
+
+defaults = read_xlsx("data/country_defaults.xlsx")
 
 # Define UI
 ui <- fluidPage(
@@ -13,23 +19,29 @@ ui <- fluidPage(
         # Sidebar to demonstrate various slider options
         sidebarPanel(
             h2("Basic stats about your farm"),
-            selectInput("country", "What country are you from?", choices = c("X", "Y"), selected = ""),
-            numericInput("flock_size", "What is the initial flock size?", value = 10000),
+            selectInput("country", "What country are you from?", choices = unique(defaults$country), selected = "China"),
+            numericInput("flock_size", "What is the initial flock size?", value = NULL),
             h2("Costs")
         ),
         
         # Main panel for displaying outputs
         mainPanel(
-            
             textOutput("flock")
         )
     )
 )
 
 # Define server logic
-server <- function(input, output) {
+server <- function(input, output, session) {
     
     output$flock = renderText(input$flock_size)
+    
+    default_flock = 
+    observeEvent(
+        input$country, {
+            updateNumericInput(session, inputId = "flock_size", value = filter(defaults, country==input$country, variable=="flock_size") %>% pull(value))
+            #updateNumericInput(session, inputId = "flock_size", value = case_when(input$country == "China" ~ 10, T ~ 500))
+    })
 }
 
 # Run the application
