9d2b963668bb64aba95ff5698fb24e68f56b23e8
djschoo <djschoo@protonmail.com>
2021-10-02 16:38:33 -0700

    facet graph added

--- app.R
+++ app.R
@@ -34,7 +34,10 @@ ui <- fluidPage(
             numericInputIcon("mortality", "What is the mortality rate of your flock?", value = NULL, min=0, max=100, step=.5, icon=list(NULL, icon("percent"))),
             numericInputIcon("growth", "What is the expected growth rate of your flock?", value = NULL, min=0, max=100, step=.5, icon=list(NULL, icon("percent"))),
             numericInputIcon("perc_laying", "What percentage of your flock lays eggs?", value = NULL, min=0, max=100, step=.5, icon=list(NULL, icon("percent"))),
-            
+            numericInput("eggs_laid", "What number of eggs does each hen lay?", value = NULL, min=0, step=1000),
+            numericInputIcon("breakage", "What percentage of eggs break?", value = NULL, min=0, max=100, step=.5, icon=list(NULL, icon("percent"))),
+            br(),
+            br(),
             
             h3("Costs")
         ),
@@ -49,26 +52,31 @@ ui <- fluidPage(
 
 # Define server logic
 server <- function(input, output, session) {
-    
+    event_vars = c("flock_size", "mortality", "growth", "perc_laying", "eggs_laid", "breakage")
     defaults = reactive(defaults_all %>% filter(country == input$country))
+    
+    observeEvent(input$country, {for (var in event_vars) updateNumericInput(session, inputId = var, value = NA)})
+
+    observeEvent(input$go, {for (var in event_vars) updateNumericInput(session, inputId = var, value = filter(defaults(), variable==var) %>% pull(value))})
+    
     fc = reactive({
         tibble(
             year = 1:input$num_years,
-            flock_size = as.integer(input$flock_size * (1 + input$growth/100 - input$mortality/100) ^ year)
-    )})
-    
-    observeEvent(input$country, {for (var in c("flock_size", "mortality", "growth", "perc_laying")) updateNumericInput(session, inputId = var, value = NA)})
-
-    observeEvent(input$go, {for (var in c("flock_size", "mortality", "growth", "perc_laying")) updateNumericInput(session, inputId = var, value = filter(defaults(), variable==var) %>% pull(value))})
+            flock_size = as.integer(input$flock_size * (1 + input$growth/100 - input$mortality/100) ^ year),
+            num_eggs = flock_size * input$perc_laying * input$eggs_laid * (1 - input$breakage/100)
+        )})
             
-    output$flock_size = renderText(input$flock_size)
     output$fc = renderReactable(reactable(fc()))
     output$g = renderPlotly(
-        ggplot(fc()) + 
-            aes(x=year, y=flock_size, group=1) + 
+        fc() %>%
+            pivot_longer(cols = 2:ncol(fc())) %>%
+            ggplot() + 
+            aes(x=year, y=value) + 
+            facet_wrap(~name, ncol=1, scales='free_y') +
             geom_line() + geom_point() +
             scale_x_continuous(breaks=fc()$year) + 
-            scale_y_continuous(labels = scales::comma)
+            scale_y_continuous(labels = scales::comma) +
+            labs(y=NULL)
     )
 }
 
