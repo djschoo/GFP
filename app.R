3504c4c91d72d6a9dbfcc9bacafd56e74ad9e58d
djschoo <djschoo@protonmail.com>
2021-10-15 16:05:03 -0700

    fixed y axis of graphs - it's still not perfect cuz I got rid of currency symbols (wasn't working with FA)

--- app.R
+++ app.R
@@ -6,6 +6,8 @@ library(readxl)
 library(reactable)
 options(scipen = 999)
 
+`%,%` = function(a,b) paste0(a,b)
+
 countries_all = read_excel("www/countries.xlsx", sheet="countries")
 countries = c("", countries_all$country)
 flags = c("", countries_all$symbol)
@@ -27,7 +29,7 @@ defaults_all = read_excel("www/countries.xlsx", sheet="defaults")
 event_vars = defaults_all$variable %>% unique()
 currency_vars = c("price_egg", "price_spent", "price_manure", "cost_feed", "cost_labor", "cost_pullet", "cost_equip", "cost_litter", "cost_vet", "cost_land", "cost_office")
 
-pl = function(df, l=scales::comma) {
+pl = function(df, l=scales::comma, ylabel=NULL) {
     df %>%
     pivot_longer(cols = 2:ncol(df)) %>%
     ggplot() + 
@@ -37,7 +39,7 @@ pl = function(df, l=scales::comma) {
     
     scale_x_continuous(breaks=df$`Year`) + 
     scale_y_continuous(labels = l) +
-    labs(y=NULL, color=NULL)
+    labs(y=ylabel, color=NULL)
 }
 
 # Define UI
@@ -159,30 +161,31 @@ server <- function(input, output, session) {
     
     flock = reactive(fc() %>% select(year, flock_size, viable_hens, spent_hens) %>% setNames(c("Year", "Flock Size", "Viable Hens", "Spent Hens")))
     output$t_flock = renderReactable(reactable(flock() %>% mutate(across(2:4, scales::comma))))
-    output$g_flock = renderPlotly(pl(flock()))
+    output$g_flock = renderPlotly(pl(flock(), ylabel="Number of Birds"))
     
     eggs = reactive(fc() %>% select(year, num_eggs, viable_eggs, broken_eggs) %>% setNames(c("Year", "Number of Eggs", "Viable Eggs", "Broken Eggs")))
     output$t_eggs = renderReactable(reactable(eggs() %>% mutate(across(2:4, scales::comma))))
-    output$g_eggs = renderPlotly(pl(eggs()))
+    output$g_eggs = renderPlotly(pl(eggs(), ylabel="Number of Eggs"))
     
     revenues = reactive(fc() %>% select(year, starts_with("revenue")) %>% setNames(c("Year", "Eggs", "Spent Hens", "Manure", "Total")))
-    output$t_revenues = renderReactable(reactable(revenues() %>% mutate(across(2:5, scales::dollar))))
-    output$g_revenues = renderPlotly(pl(revenues(), l = scales::dollar))
+    output$t_revenues = renderReactable(reactable(revenues() %>% mutate(across(2:5, scales::comma))))
+    output$g_revenues = renderPlotly(pl(revenues(), ylabel="Revenues"))
     
     totals = reactive(fc() %>% select(year, cost_total, revenue_total, profit) %>% setNames(c("Year", "Total Cost", "Total Revenue", "Total Profit")))
-    output$t_totals = renderReactable(reactable(totals() %>% mutate(across(2:4, scales::dollar))))
-    output$g_totals = renderPlotly(
-        totals() %>%
-            pivot_longer(cols = 2:4) %>%
-            ggplot() + 
-            theme_light() +
-            aes(x=`Year`, y=value, color=name) + 
-            geom_line() + geom_point() +
-            geom_ribbon(aes(ymax=value*1.05, ymin=value*.95, alpha=.1, fill=name)) +
-            scale_x_continuous(breaks=totals()$`Year`) + 
-            scale_y_continuous(labels = scales::dollar) +
-            labs(y=NULL, color=NULL)
-    )
+    output$t_totals = renderReactable(reactable(totals() %>% mutate(across(2:4, scales::comma))))
+    output$g_totals = renderPlotly(pl(totals(), ylabel="Cost/Revenue/Profit"))
+    # output$g_totals = renderPlotly(
+    #     totals() %>%
+    #         pivot_longer(cols = 2:4) %>%
+    #         ggplot() + 
+    #         theme_light() +
+    #         aes(x=`Year`, y=value, color=name) + 
+    #         geom_line() + geom_point() +
+    #         geom_ribbon(aes(ymax=value*1.05, ymin=value*.95, alpha=.1, fill=name)) +
+    #         scale_x_continuous(breaks=totals()$`Year`) + 
+    #         scale_y_continuous(labels = scales::comma) +
+    #         labs(y=NULL, color=NULL)
+    # )
 }
 
 # Run the application
