049fa4ae53f6c4d1d1f516f427be8269a0545199
djschoo <djschoo@protonmail.com>
2022-02-10 19:12:19 -0800

    did most of hannah's things - one or two small things to sort out

--- app.R
+++ app.R
@@ -55,9 +55,9 @@ ui <- fluidPage(
       h3("Basic Statistics"),
       numericInputIcon("flock_size", info_icon("Flock size (number of birds)", blurbs["flock_size"]), value = NULL, min=0, step=1000),
       numericInputIcon("mortality", info_icon("Mortality rate (%)", blurbs['mortality']), value = NULL, min=0, max=100, step=.5, icon=list(NULL, icon("percent"))),
-      sliderInput("period_length", info_icon("Laying period (months)", blurbs['period_length']), value = 12, min=1, max=20, step=1),
+      sliderInput("period_length", info_icon("Laying period (months)", blurbs['period_length']), value = 12, min=1, max=24, step=1),
       numericInputIcon("lay_percent", info_icon("Average rate of lay (%)", blurbs['lay_percent']), value = NULL, min=0, max=100, step=.5, icon=list(NULL, icon("percent"))),
-      sliderInput("transition_length", info_icon("Down Time Between Flocks (Months)", blurbs['transition_length']), value = 2, min=1, max=10, step=1),
+      sliderInput("transition_length", info_icon("Down Time Between Flocks (Months)", blurbs['transition_length']), value = 2, min=1, max=12, step=1),
       numericInputIcon("breakage", info_icon("Eggs lost (%)", blurbs['breakage']), value = NULL, min=0, max=100, step=.5, icon=list(NULL, icon("percent"))),
       
       h3("Revenues"),
@@ -66,19 +66,19 @@ ui <- fluidPage(
       numericInputIcon("revenue_manure", info_icon("Sale of manure (per flock)", blurbs['revenue_manure']), value = NULL, min=0L, step=1, icon = icon("dollar", verify_fa=F)),
       
       h3("Variable Costs"),
-      numericInputIcon("cost_feed", info_icon("Feed costs (per month)", blurbs['cost_feed']), value = NULL, min=0, max=5000, step=.5, icon = icon("dollar", verify_fa=F)),
-      numericInputIcon("cost_labor", info_icon("Labour costs (per month)", blurbs['cost_labor']), value = NULL, min=0, max=5000, step=.5, icon = icon("dollar", verify_fa=F)),
-      numericInputIcon("cost_equip", info_icon("Equipment & Maintanance (per year)", blurbs['cost_equip']), value = NULL, min=0, max=5000, step=.5, icon = icon("dollar", verify_fa=F)),
-      numericInputIcon("cost_pullet", info_icon("Replacement pullets (per pullet)", blurbs['cost_pullet']), value = NULL, min=0, max=5000, step=.5, icon = icon("dollar", verify_fa=F)),
-      numericInputIcon("cost_litter", info_icon("Litter costs (per flock)", blurbs['cost_litter']), value = NULL, min=0, max=5000, step=.5, icon = icon("dollar", verify_fa=F)),
-      numericInputIcon("cost_vet", info_icon("Veterinary care and medications (per flock)", blurbs['cost_vet']), value = NULL, min=0, max=5000, step=.5, icon = icon("dollar", verify_fa=F)),
+      numericInputIcon("cost_feed", info_icon("Feed costs (per month)", blurbs['cost_feed']), value = NULL, min=0, step=.5, icon = icon("dollar", verify_fa=F)),
+      numericInputIcon("cost_labor", info_icon("Labour costs (per month)", blurbs['cost_labor']), value = NULL, min=0, step=.5, icon = icon("dollar", verify_fa=F)),
+      numericInputIcon("cost_equip", info_icon("Equipment & Maintanance (per year)", blurbs['cost_equip']), value = NULL, min=0, step=.5, icon = icon("dollar", verify_fa=F)),
+      numericInputIcon("cost_pullet", info_icon("Replacement pullets (per pullet)", blurbs['cost_pullet']), value = NULL, min=0, step=.5, icon = icon("dollar", verify_fa=F)),
+      numericInputIcon("cost_litter", info_icon("Litter costs (per flock)", blurbs['cost_litter']), value = NULL, min=0, step=.5, icon = icon("dollar", verify_fa=F)),
+      numericInputIcon("cost_vet", info_icon("Veterinary care and medications (per flock)", blurbs['cost_vet']), value = NULL, min=0, step=.5, icon = icon("dollar", verify_fa=F)),
       numericInputIcon("cost_utilities", info_icon("Utilities costs (per year)
-", blurbs['cost_utilities']), value = NULL, min=0, max=5000, step=.5, icon = icon("dollar", verify_fa=F)),
-      numericInputIcon("cost_other", info_icon("Other costs (per year)", blurbs['cost_other']), value = NULL, min=0, max=5000, step=.5, icon = icon("dollar", verify_fa=F)),
+", blurbs['cost_utilities']), value = NULL, min=0, step=.5, icon = icon("dollar", verify_fa=F)),
+      numericInputIcon("cost_other", info_icon("Other costs (per year)", blurbs['cost_other']), value = NULL, min=0, step=.5, icon = icon("dollar", verify_fa=F)),
       
       h3("Fixed Costs"),
-      numericInputIcon("fixed_land", info_icon("Land (yearly)", blurbs['fixed_land']), value = NULL, min=0, max=50000, step=.5, icon = icon("dollar", verify_fa=F)),
-      numericInputIcon("fixed_other", info_icon("Other fixed costs (yearly)", blurbs['fixed_other']), value = NULL, min=0, max=50000, step=.5, icon = icon("dollar", verify_fa=F))),
+      numericInputIcon("fixed_land", info_icon("Land (yearly)", blurbs['fixed_land']), value = NULL, min=0, step=.5, icon = icon("dollar", verify_fa=F)),
+      numericInputIcon("fixed_other", info_icon("Other fixed costs (yearly)", blurbs['fixed_other']), value = NULL, min=0, step=.5, icon = icon("dollar", verify_fa=F))),
     
     # Main panel for displaying outputs
     mainPanel(
@@ -133,14 +133,14 @@ server <- function(input, output, session) {
             period_rank == 1 ~ as.double(input$flock_size),
             is_transition ~ 0.0,
             T ~ input$flock_size * (survival() ^ (period_rank - 1))),
-          num_eggs = num_hens * 30.5 * (1 - input$breakage / 100) * input$lay_percent,
+          num_eggs = num_hens * 30.5 * (1 - input$breakage / 100) * input$lay_percent / 100,
           revenue_eggs = num_eggs * input$price_egg,
           revenue_spent = case_when(period_rank == input$period_length + 1 ~ lag(num_hens) * input$price_spent, T ~ 0),
           revenue_manure = case_when(period_rank == input$period_length + 1 ~ as.double(input$revenue_manure), T ~ 0.0),
           cost_feed = case_when(!is_transition ~ as.double(input$cost_feed), T ~ 0.0),
           cost_labor = as.double(input$cost_labor),
           cost_equip = input$cost_equip / 12,
-          cost_pullet = case_when(is_transition & !lead(is_transition) ~ as.double(input$cost_pullet * input$flock_size), T ~ 0.0),
+          cost_pullet = case_when((year == 1 & period == 1) | (is_transition & !lead(is_transition)) ~ as.double(input$cost_pullet * input$flock_size), T ~ 0.0),
           cost_litter = case_when(period_rank == 1 ~ as.double(input$cost_litter), T ~ 0.0),
           cost_vet = case_when(!is_transition ~ input$cost_vet / input$period_length, T ~ 0.0),
           cost_utilities = input$cost_utilities / 12,
@@ -182,15 +182,15 @@ server <- function(input, output, session) {
     output$g_revenue = renderPlotly(revenue() %>%
       select(-`Total Revenue`) %>%
       pivot_longer(cols = 2:5) %>%
-      mutate(facet = case_when(name == "Number of Eggs" ~ "Eggs", T ~ "Revenues")) %>%
+      mutate(facet = case_when(name %in% c("Revenue from Spent Hens", "Revenue from Manure") ~ "Revenue from Spent Hens and Manure", T ~ name)) %>%
       ggplot() +
       theme_light() +
       aes(x=`Year`, y=value, color=name) +
       geom_line() + geom_point() +
       scale_x_continuous(breaks=revenue()$`Year`) +
       scale_y_continuous(labels = scales::comma) +
-      facet_wrap(~facet, scales="free_y", ncol=1) + 
-      labs(y=currency_text(), color=NULL))
+      facet_wrap(~facet, scales="free_y", ncol=1) +
+      labs(y=currency_text() %,% "\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t" %,% currency_text() %,% "\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t" %,% "Eggs", color=NULL))
     
     output$d_revenue = downloadHandler(filename = "revenue_data.csv", content = function(file) write.csv(revenue(), file, row.names = FALSE))
     
